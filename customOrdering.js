c98fe274546a78f6dc4559c73dc73fc56834daa5
reenaalphonic <reena.alphonic.in>
2022-11-03 14:32:54 +0530

    commit

---
+++ customOrdering.js
@@ -0,0 +1,191 @@
+const doc =  [
+        {
+            "_id": "62c3ca2faeafa1548ce069c1",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "vicky@gmail.com",
+            "firstName": "chaman",
+            "lastName": "sharma",
+            "code": "92",
+            "phone": "9982720527",
+            "referralCode": "RV6CR3DG",
+            "status": "VERIFIED",
+            "createdAt": "2022-07-05T05:20:47.405Z",
+            "updatedAt": "2022-07-05T05:20:47.405Z",
+            "__v": 0
+        },
+        {
+            "_id": "62c3badb06a032df43a1c0e9",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "reenag@gmail.com",
+            "firstName": "reena",
+            "lastName": "mehta",
+            "code": "+91",
+            "phone": "3052876402",
+            "referralCode": "9L8NHLZJ",
+            "status": "VERIFIED",
+            "createdAt": "2022-07-05T04:15:23.831Z",
+            "updatedAt": "2022-07-05T04:15:23.831Z",
+            "__v": 0
+        },
+        {
+            "_id": "62bd45056066722ee1844d2c",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "albert@mailinator.com",
+            "firstName": "Kabeer..",
+            "lastName": "Singh ",
+            "code": "+91",
+            "phone": "9413183778",
+            "referralCode": "2Y4J7YT7",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-30T06:39:01.957Z",
+            "updatedAt": "2022-06-30T11:03:35.158Z",
+            "__v": 0
+        },
+        {
+            "_id": "62bc125744af58d2da69dff6",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "krvivek117@gmail.com",
+            "firstName": "Rohan",
+            "lastName": "Choudhary",
+            "code": "+91",
+            "phone": "9024091793",
+            "referralCode": "TXI0H3L1",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-29T08:50:31.796Z",
+            "updatedAt": "2022-06-29T08:50:31.796Z",
+            "__v": 0
+        },
+        {
+            "_id": "62bbf47aa13fb32f64477896",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "vikastevatiya18@gmail.com",
+            "firstName": "subham",
+            "lastName": "kumar",
+            "code": "+91",
+            "phone": "9132247434",
+            "referralCode": "Q8BTDKRI",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-29T06:43:06.056Z",
+            "updatedAt": "2022-06-29T06:43:06.056Z",
+            "__v": 0
+        },
+        {
+            "_id": "62bbe4a40286cbe18e901dc9",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": false,
+            "isEmailVerified": false,
+            "email": "kabeer@mailiator.com",
+            "firstName": "Kabeer",
+            "lastName": "Singh ",
+            "code": "+91",
+            "phone": "9413184778",
+            "referralCode": "XKK554S1",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-29T05:35:32.475Z",
+            "updatedAt": "2022-06-29T06:18:25.193Z",
+            "__v": 0
+        },
+        {
+            "_id": "62bac53955eeb2a45f833291",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "vikastevatiya1111@gmail.com",
+            "firstName": "rohan",
+            "lastName": "kumar",
+            "code": "+91",
+            "phone": "9981720517",
+            "referralCode": "BG62LH5L",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-28T09:09:13.181Z",
+            "updatedAt": "2022-06-28T09:09:13.181Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b56739b57cf32632e35d25",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "reenagmail.com",
+            "firstName": "reena",
+            "lastName": "mehta",
+            "code": "+91",
+            "phone": "3052976402",
+            "referralCode": "D6AAETKC",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-24T07:26:49.822Z",
+            "updatedAt": "2022-06-24T07:26:49.822Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b1a7ac4bf1095f5c27d6ce",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "aj196@gmail.com",
+            "firstName": "aghazj",
+            "lastName": "junaid",
+            "code": "+91",
+            "phone": "7992367520",
+            "referralCode": "C4RE7RJL",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-21T11:12:44.619Z",
+            "updatedAt": "2022-06-21T11:12:44.619Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b19dc0f6ef46cb79cc5633",
+            "agree_box": true,
+            "image": "303030303030303030303030",
+            "isPhoneVerified": true,
+            "isEmailVerified": false,
+            "email": "subayan@alphonic.in",
+            "firstName": "Subayan",
+            "lastName": "Sen Gupta",
+            "code": "+91",
+            "phone": "9785172130",
+            "referralCode": "6FLLZ51N",
+            "status": "VERIFIED",
+            "createdAt": "2022-06-21T10:30:24.308Z",
+            "updatedAt": "2022-06-21T10:30:24.308Z",
+            "__v": 0
+        }
+    ]
+   
+ let order = ["firstName" ,"lastName"]
+
+function sortObj(doc, order) {
+    const newDoc = [];
+    
+  
+    for (let obj of doc) {
+    
+      const sortedObj = JSON.parse(JSON.stringify(obj, order));
+    //   console.log(sortedObj);
+      newDoc.push(sortedObj);
+    }
+  
+    return newDoc;
+}
+
+
+console.log(sortObj(doc , order))
